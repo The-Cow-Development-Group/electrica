0f48bae7bbfdac946777f7668979ddce45cf3813
thecow275 <thecow275@github.git>
2017-06-21 01:49:32 +0300

    added bronze and made the code a lil bit cleaner and easier to read

--- electrica/craftitems.lua
+++ electrica/craftitems.lua
@@ -1,17 +1,26 @@
 print("Electrica Craftitems is loading please wait")
-minetest.register_craftitem("electrica:cobalt_ingot", {
+
+minetest.register_craftitem("electrica:cobalt_ingot", { --[cobalt}--
 	description = "Cobalt Ingot",
 	inventory_image = "electrica_cobalt_ingot.png"
 })
-minetest.register_craftitem("electrica:charcoal_lump", {
+
+minetest.register_craftitem("electrica:bronze_ingot", { --[bronze}--
+	description = "Bronze Ingot",
+	inventory_image = "electrica_bronze_ingot.png"
+})
+
+minetest.register_craftitem("electrica:charcoal_lump", { --[charcoal lump}--
 	description = "Charcoal Lump",
 	inventory_image = "electrica_charcoal_lump.png"
 })
-minetest.register_craftitem("electrica:steel_gear", {
+
+minetest.register_craftitem("electrica:steel_gear", { --[steel gear}--
 	description = "Steel Gear",
 	inventory_image = "electrica_iron_gear.png"
 })
-minetest.register_craftitem("electrica:quartz_lump", {
+
+minetest.register_craftitem("electrica:quartz_lump", { --[quartz lump}--
 	description = "quartz",
 	inventory_image = "electrica_quartz.png"
 })
