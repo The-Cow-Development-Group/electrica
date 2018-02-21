2bd1fa0c1e9f1c2c91c245ede42ddeddc6cf09e4
Evil Gabe <thecow275@users.noreply.github.com>
2018-02-21 14:26:33 +0200

    fixes and grouping changes
    WARNING GROUP WRENCHY WILL CRASH GAME IF USED INCORRECTLY AS TOOL WHEN NOT A TOOL

--- electrica_plastic/craftitems.lua
+++ electrica_plastic/craftitems.lua
@@ -1,9 +1,9 @@
- minetest.register_craftitem("electrica_plastic:bucket_silicon", { --[bucket of plastic]--
+ minetest.register_craftitem("electrica_plastic:bucket_silicon", { --[bucket of silicon]--
 	description = "(BETA) Bucket Of Liguid Silicon",
 	inventory_image = "electrica_plastic_plastic.png"
 })
-minetest.register_craftitem("electrica_plastic:silicon_ingot", { --[plastic ingot}--
+minetest.register_craftitem("electrica_plastic:silicon_ingot", { --[silicon ingot}-- --[should propably make bouncy]--
 	description = "Silicon Ingot",
 	inventory_image = "electrica_plastic_plastic_ingot.png",
-	groups = {cracky = 1, dye=1, basecolor_cyan=1, excolor_cyan=1, unicolor_cyan=1},
+	groups = {cracky = 1, dye=1, basecolor_cyan=1, excolor_cyan=1, unicolor_cyan=1, wrenchy=1},
 })
