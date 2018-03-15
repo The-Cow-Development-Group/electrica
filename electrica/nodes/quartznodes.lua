35a06402f6e851357753dddadf0da05efff72c29
Evil Gabe <thecow275@users.noreply.github.com>
2018-03-15 10:00:34 +0200

    Update quartznodes.lua

--- electrica/nodes/quartznodes.lua
+++ electrica/nodes/quartznodes.lua
@@ -1,29 +1,25 @@
-minetest.register_node("electrica:quartz_base", {
-
-description = "Quartz Base",
-
-tiles = {"electrica_quartz_node_top.png", "electrica_quartz_node_top.png", "electrica_quartz_node_side.png", "electrica_quartz_node_side.png", "electrica_quartz_node_side.png", "electrica_quartz_node_side.png"},
-
+minetest.register_node("electrica:quartz_pillar", {
+description = "Quartz Pillar",
+tiles = {"electrica_quartz_pillar_top.png", "electrica_quartz_pillar_top.png", "electrica_quartz_pillar_side.png", "electrica_quartz_pillar_side.png", "electrica_quartz_pillar_side.png", "electrica_quartz_pillar_side.png"},
 paramtype2 = "facedir",
-
 groups = {cracky = 1},
+sounds = default.node_sound_metal_defaults(),
+on_place = minetest.rotate_node,
+})
 
+minetest.register_node("electrica:quartz_base", {
+description = "Quartz Base",
+tiles = {"electrica_quartz_node_top.png", "electrica_quartz_node_top.png", "electrica_quartz_node_side.png", "electrica_quartz_node_side.png", "electrica_quartz_node_side.png", "electrica_quartz_node_side.png"},
+paramtype2 = "facedir",
+groups = {cracky = 1},
 sounds = default.node_sound_stone_defaults(),
-
 -- on_place = minetest.rotate_node,
-
 })
 
 minetest.register_node("electrica:gravity_quartz_brick", {
-
 description = "(WIP) (BETA) Gravity Quartz Brick",
-
 is_ground_content = true,
-
 tiles = {"electrica_quartz_brick.png"},
-
 sounds = default.node_sound_metal_defaults(),
-
 groups = {cracky = 1, falling_node = 1, sand = 1, stone = 1, stone = 2},
-
 })
