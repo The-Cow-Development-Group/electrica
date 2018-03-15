91be63e6453b1a2039e0322cb1eacadaf945333d
Evil Gabe <thecow275@users.noreply.github.com>
2018-03-15 10:01:36 +0200

    Update quartznodes.lua

--- electrica/nodes/quartznodes.lua
+++ electrica/nodes/quartznodes.lua
@@ -1,3 +1,12 @@
+minetest.register_node("electrica:quartz_block", {
+description = "Quartz Block",
+tiles = {"electrica_quartz_node_top.png", "electrica_quartz_node_top.png", "electrica_quartz_node_side.png", "electrica_quartz_node_side.png", "electrica_quartz_node_side.png", "electrica_quartz_node_side.png"},
+paramtype2 = "facedir",
+groups = {wrenchy = 1},
+sounds = default.node_sound_stone_defaults(),
+on_place = minetest.rotate_node,
+})
+
 minetest.register_node("electrica:quartz_pillar", {
 description = "Quartz Pillar",
 tiles = {"electrica_quartz_pillar_top.png", "electrica_quartz_pillar_top.png", "electrica_quartz_pillar_side.png", "electrica_quartz_pillar_side.png", "electrica_quartz_pillar_side.png", "electrica_quartz_pillar_side.png"},
