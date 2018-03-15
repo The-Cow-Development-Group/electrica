2f0bebd9d41a22911d9e1f3efd14864cf9b01630
Evil Gabe <thecow275@users.noreply.github.com>
2018-03-15 10:02:41 +0200

    Update quartznodes.lua

--- electrica/nodes/quartznodes.lua
+++ electrica/nodes/quartznodes.lua
@@ -32,3 +32,11 @@ tiles = {"electrica_quartz_brick.png"},
 sounds = default.node_sound_metal_defaults(),
 groups = {cracky = 1, falling_node = 1, sand = 1, stone = 1, stone = 2},
 })
+
+minetest.register_node("electrica:quartz_brick", {
+description = "(WIP) Quartz Brick",
+is_ground_content = true,
+tiles = {"electrica_quartz_brick.png"},
+groups = {cracky = 1},
+sounds = default.node_sound_metal_defaults(),
+})
