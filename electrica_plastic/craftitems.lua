9ef9afd14b1b26f5df998d7f5bec3e355f50f696
Evil Gabe <thecow275@users.noreply.github.com>
2018-04-25 09:36:37 +0300

    Update craftitems.lua
    Added missing items

--- electrica_plastic/craftitems.lua
+++ electrica_plastic/craftitems.lua
@@ -1,11 +1,15 @@
- minetest.register_craftitem("electrica_plastic:bucket_silicon", { --[bucket of silicon]--
+minetest.register_craftitem("electrica_plastic:bucket_silicon", { --[bucket of Silicon]--
 	description = "(BETA) Bucket Of Liguid Silicon",
 	inventory_image = "electrica_plastic_plastic.png"
 })
-minetest.register_craftitem("electrica_plastic:silicon_ingot", { --[silicon ingot}-- --[should propably make bouncy]--
+minetest.register_craftitem("electrica_plastic:silicon_ingot", { --[plastic ingot}--
 	description = "Silicon Ingot",
 	inventory_image = "electrica_plastic_plastic_ingot.png",
-	groups = {cracky = 1, dye=1, basecolor_cyan=1, excolor_black=1, unicolor_black=1, wrenchy=1},
+	groups = {cracky = 1, dye=1, basecolor_cyan=1, excolor_cyan=1, unicolor_cyan=1},
+})
+minetest.register_craftitem("electrica_plastic:bucket_plastidip", { --[bucket of plastidip]--
+	description = "(BETA) Bucket Of Plastidip",
+	inventory_image = "electrica_plastic_plastic.png"
 })
 minetest.register_craftitem("electrica_plastic:nylon_ingot", { --[nylonon ingot}-- --[should propably make Mallulable]--
 	description = "Nylon Ingot",
