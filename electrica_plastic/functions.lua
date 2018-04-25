bf22bfe1ab3f4166f0642927daecf7648bcfa234
Evil Gabe <thecow275@users.noreply.github.com>
2018-04-25 09:51:18 +0300

    Update functions.lua
    started renaming to Silicon from plastic

--- electrica_plastic/functions.lua
+++ electrica_plastic/functions.lua
@@ -2,7 +2,7 @@ bucket.register_liquid(
 	"electrica_plastic:plastic_source", -- source node --
 	"electrica_plastic:plastic_flowing", -- flowing node --
 	"electrica_plastic:bucket_plastic", -- the new bucket item --
-	"bucket_plastic.png", -- texture of the new bucket item --
-	"Plastic Bucket",     -- name of the bucket --
+	"bucket_plastic_new.png", -- texture of the new bucket item --
+	"Bucket of Silicon",     -- name of the bucket --
 	{plastic_bucket = 1}  -- groups --
 )
