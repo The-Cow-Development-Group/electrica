d7263d375afce2a7feba203550a9ce77c75a77d2
Evil Gabe <thecow275@users.noreply.github.com>
2018-04-25 09:34:29 +0300

    Update gearboxes.lua
    added broken gearboxes fully but still broken due to no implementation

--- electrica/nodes/gearboxes.lua
+++ electrica/nodes/gearboxes.lua
@@ -2,10 +2,9 @@ minetest.register_node("electrica:gearbox_wooden", { --Wooden Gearbox
 
 description = "Wooden Gearbox",
 
-tiles = {"electrica_quartz_node_top.png", "electrica_quartz_node_top.png", "electrica_quartz_node_side.png", "electrica_quartz_node_side.png", "electrica_quartz_node_side.png", "electrica_quartz_node_side.png"},
---[ Textures are not right yet ]--
+tiles = {"electrica_gearbox_wooden_top.png", "electrica_gearbox_wooden_top.png", "electrica_gearbox_wooden_side.png", "electrica_gearbox_wooden_side.png", "electrica_gearbox_wooden_side.png", "electrica_gearbox_wooden_side.png"},
 paramtype2 = "facedir",
-
+--[Textures electrica_gearbox_wooden_top.png and electrica_gearbox_wooden_side.png ]--
 groups = {wrenchy = 1, cracky = 1},
 
 sounds = default.node_sound_stone_defaults(),
@@ -15,17 +14,32 @@ on_place = minetest.rotate_node,
 })
 
 minetest.register_node("electrica:gearbox_copper", { --Copper Gearbox
-
 description = "Copper Gearbox",
+tiles = {"electrica_gearbox_copper_top.png", "electrica_gearbox_copper_top.png", "electrica_gearbox_copper_side.png", "electrica_gearbox_copper_side.png", "electrica_gearbox_copper_side.png", "electrica_gearbox_wooden_side.png"},
+--[ Textures electrica_gearbox_copper_top.png and electrica_gearbox_copper_side.png ]--
 
-tiles = {"electrica_quartz_node_top.png", "electrica_quartz_node_top.png", "electrica_quartz_node_side.png", "electrica_quartz_node_side.png", "electrica_quartz_node_side.png", "electrica_quartz_node_side.png"},
---[ Textures are not done yet ]--
 paramtype2 = "facedir",
-
 groups = {wrenchy = 1, cracky = 1},
-
 sounds = default.node_sound_stone_defaults(),
-
 on_place = minetest.rotate_node,
 
 })
+minetest.register_node("electrica:gearbox_iron", {
+    description = "Iron Gearbox",
+    tiles = {"default_wood.png^electrica_gearbox_iron_top.png","default_wood.png^electrica_gearbox_iron_top.png", "default_wood.png^electrica_gearbox_iron_side.png", "default_wood.png^electrica_gearbox_iron_side.png", "default_wood.png^electrica_gearbox_iron_side.png", "default_wood.png^electrica_gearbox_iron_side.png",
+      "electrica_gearbox_iron_side.png"},
+    
+   paramtype2 = "facedir",
+   groups = {wrenchy = 1, cracky = 1},
+   sounds = default.node_sound_stone_defaults(),
+   on_place = minetest.rotate_node,
+})
+
+minetest.register_node("electrica:debug_gearbox", {
+    description = "Debug Gearbox",
+    tiles = {"electrica_debug.png"},
+    paramtype2 = "facedir",
+    groups = {debug = 1},
+    sounds = default.node_sound_stone_defaults(),
+    on_place = minetest.rotate_node,
+    })
